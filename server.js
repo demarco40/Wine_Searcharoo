7775b2e191d50580a5b976b1367784ed98afbcc5
demarco40 <jxr6600@rit.edu>
2018-04-06 15:36:09 -0400

    Added base file for server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
 const app = express();
-app.use(express.static("../Contemporary")); //This assumes the folder name this file is inside is named Contemporary. Change is needed.
+app.use(express.static("../Wine_Searcharoo")); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(require('./routes/pokemon')); //if pokemon.js is in a different location, change the path to the correct location.
+app.use(require('./routes/backend_wine'));
 
-//Uses the port 3000 and sends a message to console.
 app.listen(3000, () => {
   console.log('listening on 3000')
 })

a8db3d9c7737c6f92acea724e3e51f5e498d5384
demarco40 <jxr6600@rit.edu>
2018-04-06 15:26:59 -0400

    shell for backend created

---
+++ routes/backend_wine.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/wine', (request, response) => {
+
+})
+
+router.put('/inventory/list_name', (request, response) => {
+    
+})
+
+module.exports = router;

6780081056076da3678b85152eff3e6fe7cf7aa7
Gavin Drabik <grd2747@g.rit.edu>
2018-04-09 11:19:10 -0400

    Starting to set up EJS
    Going to have to make some sllight changes to deal with the tabs

---
+++ JS/business.js
@@ -0,0 +1,21 @@
+function makeSearch() {
+console.log("here search");
+}
+
+function makeInventoy() {
+    console.log("here inventory");
+
+}
+
+function makeWishlist() {
+    console.log("here wishlist");
+
+}
+function makeFavorites() {
+    console.log("here favs");
+
+}
+function makeCustomWine() {
+    console.log("here custom");
+
+}
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const app = express();
-app.use(express.static("../Wine_Searcharoo")); 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(require('./routes/backend_wine'));
+const bodyParser = require('body-parser');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    //Do database stuff to get all information we need
+    //pass it into render
+    res.render('pages/index');
+});
+
+
+ app.use(express.static("../Wine_Searcharoo"));
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(require('./routes/backend_wine'));
 
 app.listen(3000, () => {
   console.log('listening on 3000')

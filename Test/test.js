5ff724d931e87a7e2daf1c568986fa74694ea12a
demarco40 <jxr6600@rit.edu>
2018-04-20 15:36:55 -0400

    added some comments on the GET tests

--- Test/test.js
+++ Test/test.js
@@ -8,42 +8,50 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Server Tests', function() {
+
+  //Tests to make sure the index page is loaded
   it('Should Return the index page', function(done){
     chai.request('http://localhost:3000')
     .get('/')
     done();
   });
 
+  //Tests to make sure the favorites partial is loaded
   it('Should Return the Favorites partial', function(done){
     chai.request('http://localhost:3000/favorites')
     .get('/views/partials/favorites')
     done();
   });
 
+  //Tests to make sure the Dialog partial is loaded
   it('Should Return the Dialog partial', function(done){
     chai.request('http://localhost:3000/dialog')
     .get('/views/partials/dialog')
     done();
   });
 
+  //Tests to make sure the Wishlist partial is loaded
   it('Should Return the Wishlist partial', function(done){
     chai.request('http://localhost:3000/wishlist')
     .get('/views/partials/wishlist')
     done();
   });
 
+  //Tests to make sure the Custom partial is loaded
   it('Should Return the Custom partial', function(done){
     chai.request('http://localhost:3000/custom')
     .get('/views/partials/custom')
     done();
   });
 
+  //Tests to make sure the Head partial is loaded
   it('Should Return the Head partial', function(done){
     chai.request('http://localhost:3000/Head')
     .get('/views/partials/head')
     done();
   });
 
+  //Tests to make sure the Search partial is loaded
   it('Should Return the Search partial', function(done){
     chai.request('http://localhost:3000/Search')
     .get('/views/partials/search')

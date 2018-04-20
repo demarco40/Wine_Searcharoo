944bcabd0464e88ed206964434f70052838314cd
Gavin Drabik <grd2747@g.rit.edu>
2018-04-20 12:07:27 -0400

    Working on data layer and api

---
+++ JS/data.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'student',
+    database: 'wine_search'
+});
+module.exports = {//this makes it so you can use the functions in another file
+    select: function (wineJson) {//slightly different naming convention
+        //console.log(wineJson);
+        var queryParams  = {wineApiCode: 'code-var-from-input'};
+        var query = connection.query('SELECT * FROM wine WHERE ?', queryParams, function(err, result) {
+        });
+    },
+    insert: function (input) {
+        //TODO: need to add vintage to database, then to query params
+        //TODO: need to make wineId auto increment in the database so we dont need to pass in a value
+
+
+        //need to check if it already exists before adding
+        var itExists = false;
+        connection.query('SELECT * FROM wine WHERE ?',{wineApiCode:input.code},function(err,result){
+            itExists = (result.length > 0) ? (true) : (false);//if somthing comes back it exists
+        });
+
+        //eventually wineID wont be there because it will auto increment
+        if (!itExists){
+            var queryParams  = {wineID: 1,wineApiCode: input.code, name: input.name, region: input.region,
+                 winery:input.winery, grape_varietal: input.varietal,price: input.price,
+                 image_url: input.image,favorite:0 };
+
+            connection.query('insert into wine set ?', queryParams, function(err, result) {
+                console.log(err);
+                console.log(result);
+            });
+        }
+    }
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var app = express();
+var dataLayer = require('./JS/data.js');
 app.use(express.static("../Wine_Searcharoo"));
 app.set('view engine', 'ejs');
 
@@ -9,14 +10,15 @@ app.use(bodyParser.json());
 app.get('/', function(req, res) {
     //Do database stuff to get all information we need
     //pass it into render
-    console.log(req.query);
+    //console.log(req.query);
     res.render('pages/index');
 });
 
 app.post('/addToDb', function(req,res){
     var wineJson = req.body['wines'][0];
 
-    console.log(wineJson);
+    //console.log(wineJson);
+    dataLayer.insert(wineJson);
     //make mysql code here using vars from wineJson
 });
 

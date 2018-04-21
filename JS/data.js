fcca111fe55941f4b685ffe6c7b1aeb9f681fe32
Gavin Drabik <grd2747@g.rit.edu>
2018-04-21 12:49:36 -0400

    Gpt rid of server error by checking if it already exists

--- JS/data.js
+++ JS/data.js
@@ -18,20 +18,21 @@ module.exports = {//this makes it so you can use the functions in another file
 
 
         //need to check if it already exists before adding
-        var itExists = false;
-        connection.query('SELECT * FROM wine WHERE ?',{wineApiCode:input.code},function(err,result){
-            itExists = (result.length > 0) ? (true) : (false);//if somthing comes back it exists
+
+        var queryExists = connection.query('SELECT * FROM wine WHERE ?',{wineApiCode:input.code},function(err,result){
+            console.log("IT EXISTS: "+result);
+            return (result.length > 0) ? (true) : (false);//if somthing comes back it exists
         });
 
         //eventually wineID wont be there because it will auto increment
-        if (!itExists){
+        if (!queryExists){
             var queryParams  = {wineApiCode: input.code, name: input.name, region: input.region,
                  winery:input.winery, grape_varietal: input.varietal,price: input.price, vintage: input.vintage,
                  image_url: input.image,favorite:0 };
 
             connection.query('insert into wine set ?', queryParams, function(err, result) {
-                console.log(err);
-                console.log(result);
+                console.log("ERROR: " + err);
+                console.log("INSERTED: "+result);
             });
         }
     }
